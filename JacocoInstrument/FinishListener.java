cf02d1534e17e74b6f220716bfa700c7bfd47ac2
Allen <1984777204@qq.com>
2022-05-25 21:12:31 +0800

    add JacocoInstrumentation class

---
+++ JacocoInstrument/FinishListener.java
@@ -0,0 +1,5 @@
+package com.amaze.filemanager.JacocoInstrument;
+
+public interface FinishListener {
+    void dumpIntermediateCoverage(String filePath);
+}
---
+++ JacocoInstrument/SMSInstrumentedReceiver.java
@@ -0,0 +1,26 @@
+package com.amaze.filemanager.JacocoInstrument;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import java.io.File;
+
+
+public class SMSInstrumentedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // write the coverage file to the internal storage, which will not require any permissions
+        // see: https://developer.android.com/training/data-storage/files
+        // the output dir usually locates at: /data/data/#{app_package_name}/files/coverage.ec
+        File coverageFile = new File(context.getFilesDir(), "coverage.ec");
+        String coverageFilePath = coverageFile.getAbsolutePath();
+
+        FinishListener mListener = new JacocoInstrumentation(coverageFilePath);
+
+        if (mListener != null) {
+            mListener.dumpIntermediateCoverage(coverageFilePath);
+        }
+    }
+}
